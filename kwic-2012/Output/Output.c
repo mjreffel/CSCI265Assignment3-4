f500570f3ab2756161494675af52d51bf7e5f45d
Mark <mjreffel@gmail.com>
2013-11-19 21:18:07 -0800

    Code worked on by group Nov 19

--- kwic-2012/Output/Output.c
+++ kwic-2012/Output/Output.c
@@ -18,24 +18,42 @@ void OUInit(void)
 
 void OUPrint(void)
 {
-	int i,j;
+	int i,j,k,w;
+    int d = 0;
 
 	for (i = 0; i < SSNumLines(); i++) {
 		int numWords = SSNumWords(i);
 		for (j = 0; j < numWords; j++) {
 			int shiftNum = SSGetShiftNum(i);
-			printf("%s",SSGetWord(i,j));
+			printf("%s\n",SSGetWord(i,j));
+            const char * temp = SSGetWord(i,j);
+            const char * first;
+            const char * second;
+            k = 0;
+            while(temp[k] != '\0')
+            {
+                first[d] = &temp[k];
+                d++;
+                //printf("%c ", temp[k]);
+                k++;
+            }
+            printf("%d \n",k);
 
 			/* no comma if line is unshifted
 			 * otherwise comma follows last word on original line
 			 */
-			if (shiftNum != 0 && j == numWords-shiftNum-1)
-				printf(",");
+			//if (shiftNum != 0 && j == numWords-shiftNum-1)
+				//printf(",");
 
 			/* space follows every word but the last */
-			if (j != SSNumWords(i)-1)
-				printf(" ");
+			//if (j != SSNumWords(i)-1)
+				//printf(" ");
 		}
+        
+        for(w = 0; w < 100; w++)
+        {
+            printf("%c ", first[w]);
+        }
 		printf("\n");
 	}
 }
