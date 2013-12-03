fc22f9e78749bb20d09e8a7834c4d338275bedcc
SalehAldawsari <saleh.aldawsari@gmail.com>
2013-12-03 11:43:48 -0800

    adding comments

--- kwic-2012/Output/Output.c
+++ kwic-2012/Output/Output.c
@@ -23,14 +23,18 @@ void OUPrint(void)
     int i,j,k,w,h,a,b,c,e;
     int d = 0;
     
-    // need to move it inside the loop and initialize it to length of num words 
-    // when done, delete the contant of the array and resize it. 
+    // temporary string to hold single line
     const char * tmpString[80];
-    const char * Fword;
+    // char variable to hold key word
+    const char * KeyWord;
+    // copy all words that will be after key word 
     const char * second[80];
+    // copy all words before key words 
     const char * first[80];
+    //calculate the time for outputing sorted words
     clock_t cstart = clock();
   	clock_t cend = 0;
+  	// loop each line
         for (i = 0; i < SSNumLines(); i++) {
           int f = 0;
     	  int s = 0;
@@ -39,12 +43,14 @@ void OUPrint(void)
           int Kcount = 0;
           int Scount = 0;
                 int numWords = SSNumWords(i);
+                // loop each line for words
                 for (j = 0; j < numWords; j++) {
                         int shiftNum = SSGetShiftNum(i);
-                        // need to store spaces in the array including comma 
+                        // copy each word in tmp
                         tmpString[j] = SSGetWord(i,j);
                 }
                
+               //divide each word in its category 
                 for(d=0; d<numWords; d++)
                 {
                 int sizeOffirst = 0;
@@ -63,43 +69,39 @@ void OUPrint(void)
                                 f++;
                 	} 
                 
-                }
-               
+                } 
+            // calculate length  and print words
             for (h = 0; h < f; h++) { 
-            int len = strlen(first[h]);
-  				
+            	int len = strlen(first[h]);
   				Fcount = Fcount + len +1;
             	printf("%s ", first[h]);
             }
-             Kcount = strlen(Fword); 
+            // length of key word 
+            Kcount = strlen(Fword); 
             printf("%s ", Fword);
-               
+            // length of second words    
             for (e = 0; e < s; e++) {
-            
-            int Slen = strlen(second[e]);
-            Scount = Scount + Slen;
-               printf("%s ", second[e]);
+            	int Slen = strlen(second[e]);
+            	Scount = Scount + Slen;
+               	printf("%s ", second[e]);
             }
         /*************/
                 printf("\n");
-                
+              // print spaces under each first word  
               for (b = 0; b < Fcount; b++){
                 printf(" ");
                 }
-               // printf(" ");
+               // print underscore under key word
                 for (c =0; c < Kcount; c++){
                 printf("-");
                 }
+                // print spaces after key word
                 for (a=0; a < Scount; a++){
                 printf(" ");
                 }
-                printf("\n");
-     //   printf(" :beforeKey: %d\n", Fcount);
-       // printf(" :Key: %d\n", Kcount);   
-       // printf(" :after: %d\n", Scount); 
-                               
+                printf("\n");                           
         }
           
        cend = clock();
-       printf ("%.3f cpu sec\n", ((double)cend - (double)cstart)* 1.0e-6);
+       //printf ("%.3f cpu sec\n", ((double)cend - (double)cstart)* 1.0e-6);
 }
